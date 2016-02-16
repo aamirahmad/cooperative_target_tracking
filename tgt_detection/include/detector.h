813b7d94c455d71a358a95216e2048daa38ce2cd
aamirahmad <aamir.iitkgp@gmail.com>
2016-02-16 11:06:50 +0100

    basic detection working. We have the target in the camera frame

--- tgt_detection/include/detector.h
+++ tgt_detection/include/detector.h
@@ -1,18 +1,22 @@
+
 #include <cstdio>
 #include <iostream>
 #include <string>
 
 #include <ros/ros.h>
+#include "sensor_msgs/Image.h"
+#include "sensor_msgs/CameraInfo.h"
+#include "image_transport/image_transport.h"
+#include <cv_bridge/cv_bridge.h>
 #include <tf/transform_broadcaster.h>
 #include <std_msgs/String.h>
 #include <std_msgs/Float32.h>
 #include <geometry_msgs/PoseStamped.h>
-#include <sensor_msgs/Image.h>
-#include <image_transport/image_transport.h>
-#include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/image_encodings.h>
 
 #include <opencv2/imgproc/imgproc.hpp>
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
 
 #include <boost/bind.hpp>
 #include <boost/ref.hpp>
@@ -31,29 +35,91 @@
 #include <visp3/io/vpVideoReader.h>
 #include <visp_ros/vpROSGrabber.h>
 
+//#define DRAW_DEBUG
+#undef DRAW_DEBUG
+
 using namespace std;
 using namespace ros;
+using namespace cv;
+namespace enc = sensor_msgs::image_encodings;
 
 class Detector
 {
     bool detectionSucces_; // /*!< true if detector detects the object in the image */
     
-    Subscriber imageSub_;
-    string imageTopic;
+    string baseImageTopic;
+    string maskImageTopic;
+    string maskedImageTopic;
+    string camInfoTopic;
+    
+    NodeHandle nh_;
+    image_transport::ImageTransport it_;
+    image_transport::Subscriber imageSub_;
+    Subscriber camInfoSub_;
+    
+    
+    int minAreaOfTargetProject;
+    double objectRealSurfaceArea;
+    double Z_obj_camfrm;
+    double X_obj_camfrm;
+    double Y_obj_camfrm;
+      
+    //camera parameters
+    double camFx, camFy;   
   
   public:
-    Detector(NodeHandle *nh_)
+    Detector(NodeHandle &_nh, char *detectorType): nh_(_nh), it_(nh_)
     {
-      nh_->getParam("IMAGE_TOPIC", imageTopic);
+      nh_.getParam("base_image_topic", baseImageTopic);
+      nh_.getParam("mask_image_topic", maskImageTopic);
+      nh_.getParam("segmented_image_topic", maskedImageTopic);
+      nh_.getParam("camInfoTopic", camInfoTopic);
+      nh_.getParam("minAreaOfTargetProject", minAreaOfTargetProject);
+      nh_.getParam("minAreaOfTargetProject", minAreaOfTargetProject);
+      nh_.getParam("objectRealSurfaceArea", objectRealSurfaceArea);
       
-      imageSub_ = nh_->subscribe<sensor_msgs::Image>(imageTopic, 1000, boost::bind(&Detector::imageCallback,this, _1));
+      ROS_INFO("detector type = %s",detectorType);
+
+	if(strcmp(detectorType,"HISTOGRAM")==0){
+	        imageSub_ = it_.subscribe(maskImageTopic, 10, boost::bind(&Detector::segmentationBasedDetection,this, _1));
+		ROS_INFO("Detection using purely a color histogram-based segmentation method with image topic = %s",maskImageTopic.c_str());	
+	}
+
+	if(strcmp(detectorType,"FEATURE")==0){
+	        imageSub_ = it_.subscribe(baseImageTopic, 10, boost::bind(&Detector::featureBasedDetection,this, _1));
+		ROS_INFO("Detection using purely a feature-based object detection method");
+	}
+  
+	if(strcmp(detectorType,"COMBINED")==0){
+	        imageSub_ = it_.subscribe(baseImageTopic, 10, boost::bind(&Detector::combinedDetection,this, _1));
+		ROS_INFO("Detection using a combination of color histogram-based segmentation process and a feature-based object detection method");
+	}
+		
+       // Other subscribers
+       camInfoSub_ = nh_.subscribe<sensor_msgs::CameraInfo>(camInfoTopic, 10,boost::bind(&Detector::storeCameraInfo,this,_1));
+
     }
     
-    /*! \brief This is the image callback. Most of the detection processing should happen here.
-      * However, each sub-step of detection, e.g., image segmentation, feature detection, etc.,  
-      * should happen in the respective functions which can be called from here
-      *  Detailed description starts here.
-      */
-    void imageCallback(const sensor_msgs::Image::ConstPtr&);
-
+    /*! \brief This is a method for detection that outputs the centroid of the largest
+     * image blob in the segmented image. Segmented image is obtained using the
+     * pal_image_segmentation package.
+     */
+    void segmentationBasedDetection(const sensor_msgs::Image::ConstPtr&);    
+    
+    /*! \brief This is a method for detection that outputs the centroid of the object'S
+     * projection in the actual image using a feature-based detection method
+     */
+    void featureBasedDetection(const sensor_msgs::Image::ConstPtr&);    
+    
+     /*! \brief This is a method for detection that combines both the segmentation and feature
+      * -based processes.
+     */
+    void combinedDetection(const sensor_msgs::Image::ConstPtr&);
+    
+     /*! \brief This is a method for reading cam info and storing the params into private variables
+      * of the class
+     */
+    void storeCameraInfo(const sensor_msgs::CameraInfo::ConstPtr&);    
+    
+    
 };
--- tgt_detection/src/detector.cpp
+++ tgt_detection/src/detector.cpp
@@ -1,9 +1,131 @@
 #include "../include/detector.h" 
 
-void Detector::imageCallback(const sensor_msgs::Image::ConstPtr& image)
+void Detector::storeCameraInfo(const sensor_msgs::CameraInfo::ConstPtr& msg)
+{
+  camFx = msg->K[0];
+  camFy = msg->K[4];
+  
+  ROS_INFO("cameraFx = %f",camFx);
+  ROS_INFO("cameraFy = %f",camFy);
+  
+  //storing these parameters only once is required 
+  camInfoSub_.shutdown();
+}
+
+
+
+void Detector::combinedDetection(const sensor_msgs::Image::ConstPtr& image)
 {
 
-  ROS_INFO("Received an Image");
+  ROS_INFO("Received a base Image");
+  
+  
+}
+
+
+void Detector::featureBasedDetection(const sensor_msgs::Image::ConstPtr& image)
+{
+
+  ROS_INFO("Received a base Image");
+  
+  
+}
+
+
+void Detector::segmentationBasedDetection(const sensor_msgs::Image::ConstPtr& image)
+{
+  //ROS_INFO("Received a mask Image");
+
+  cv_bridge::CvImagePtr cv_ptr;
+  
+  try
+  {
+      cv_ptr = cv_bridge::toCvCopy(image, "mono8");
+  }
+  catch (cv_bridge::Exception& e)
+  {
+      //if there is an error during conversion, display it
+      ROS_ERROR("Detector error when reading image on the segmentation mask image topic: %s", e.what());
+      return;
+  }    
+   
+  int i = 0, count = 0;
+  
+  vector<vector<Point> > contour_;
+  vector<Vec4i> hierarchy;
+  
+  findContours ( cv_ptr->image , contour_, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+
+  #ifdef DRAW_DEBUG  
+    Mat dst = Mat::zeros(cv_ptr->image.size(),CV_8UC3);
+  #endif
+      
+  int idx = 0, largestContourIndex = 0;
+  double areaOfLargestContour = 0; 
+  
+  // If we found at least one contour
+  if(contour_.size()>0)
+  {
+    // find the biggest contour
+    for( ; idx >= 0; idx = hierarchy[idx][0] )
+    {
+	const vector<Point>& c = contour_[idx];
+	double area = fabs(contourArea(Mat(c)));
+	if( area > areaOfLargestContour )
+	{
+	    areaOfLargestContour = area;
+	    largestContourIndex = idx;
+	}	
+    }
+  
+    if(areaOfLargestContour > minAreaOfTargetProject)
+    {
+      // find the convex hull
+      vector<vector<Point> >hull(1);
+      convexHull( Mat(contour_[largestContourIndex]), hull[0], false );
+      
+      /// Find the rotated rectangles and ellipses for each contour
+      vector<RotatedRect> minRect(1);
+      vector<RotatedRect> minEllipse(1);  
+      
+      minRect[0] = minAreaRect( Mat(contour_[largestContourIndex]) );
+	  if( contour_[largestContourIndex].size() > 5 )
+	    { minEllipse[i] = fitEllipse(Mat(contour_[largestContourIndex])); }
+	    
+      Point2f objectCenterIm = minRect[0].center;
+      double objectSizeInImage = minRect[0].size.width*minRect[0].size.height;
+      
+      Z_obj_camfrm = pow((camFx*camFy*objectRealSurfaceArea/objectSizeInImage),0.5);
+      X_obj_camfrm = Z_obj_camfrm*objectCenterIm.x/camFx;
+      Y_obj_camfrm = Z_obj_camfrm*objectCenterIm.y/camFy;
+      
+      //std::cout<<"X_obj_camfrm in meter = "<<X_obj_camfrm<<std::endl;
+      //std::cout<<"Y_obj_camfrm in meter = "<<Y_obj_camfrm<<std::endl;
+      //std::cout<<"Z_obj_camfrm in meter = "<<Z_obj_camfrm<<std::endl;      
+
+      
+    #ifdef DRAW_DEBUG
+	Scalar color( rand()&255, rand()&255, rand()&255 );
+	//drawContours( dst, contour_, largestContourIndex, color, 1, 8, hierarchy );
+	
+	Scalar color1( rand()&255, rand()&255, rand()&255 );
+	//drawContours( dst, hull, i, color1, 1, 8, vector<Vec4i>(), 0, Point() );
+	
+	Scalar color2( rand()&255, rand()&255, rand()&255 );
+	//ellipse( dst, minEllipse[i], color2, 2, 8 );
+	// rotated rectangle
+	Scalar color3( rand()&255, rand()&255, rand()&255 );
+	Point2f rect_points[4]; minRect[0].points(rect_points);
+	for( int j = 0; j < 4; j++ )
+	  line( dst, rect_points[j], rect_points[(j+1)%4], color3, 1, 8 );
+	namedWindow( "Components", 1 );
+	imshow( "Components", dst );
+	waitKey(2);	
+    #endif    
+	
+    }
+  }
+    
   
 }
 
@@ -11,12 +133,21 @@ int main(int argc, char* argv[])
 {
   
   ros::init(argc, argv, "tgt_detector");
-  
-  /* Do the argc argv thing here if you need to */
-  
+
+  if (argc < 2)
+    {
+      ROS_WARN("WARNING: you should at least specify the detector type as 'HISTOGRAM' or 'FEATURE' or 'COMBINED' of \n");
+      return 1;
+    }
+  else
+  {
+    ROS_INFO("Detector provided = %s",argv[1]);
+  }
+      
+    
   NodeHandle nh;
-  
-  Detector node(&nh);
+  Detector node(nh,argv[1]);
+    
   spin();
   
   return 0;  
